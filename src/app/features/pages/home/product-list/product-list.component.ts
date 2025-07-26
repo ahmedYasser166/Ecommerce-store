c0038da7cc8078cd2a5fe7b025f55d4b59eecf5c
ahmedYasseer166 <ahmed01020269254@gmail.com>
2025-07-26 21:03:07 +0300

    commit1

--- src/app/features/pages/home/product-list/product-list.component.ts
+++ src/app/features/pages/home/product-list/product-list.component.ts
@@ -3,34 +3,60 @@ import { ProductService } from '../../../../core/services/product.service';
 import { Product } from '../../../../core/models/interfaces/product';
 import { CommonModule } from '@angular/common';
 import { ProductItemComponent } from '../../../../shared/ui/product-item/product-item.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-product-list',
-  imports: [CommonModule, ProductItemComponent],
+  imports: [CommonModule, ProductItemComponent, FormsModule],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css',
 })
 export class ProductListComponent {
   private readonly productService = inject(ProductService);
-  product!: Product;
-hasError = false;
 
-  products!: Product[];
+  products: Product[] = [];
+  filteredProducts: Product[] = [];
 
-getAllProduct() {
-  this.productService.getAllProduct().subscribe({
-    next: (res) => {
-      this.products = res;
-    },
-    error: (err) => {
- console.error('An error occurred while fetching products:', err);
-  this.hasError = true;
-    },
-  });
-}
-
+  searchTerm: string = '';
+  sortOption: string = '';
+  hasError: boolean = false;
 
   ngOnInit(): void {
     this.getAllProduct();
+  }
+
+  getAllProduct() {
+    this.productService.getAllProduct().subscribe({
+      next: (res) => {
+        this.products = res;
+        this.filteredProducts = [...res];
+      },
+      error: () => {
+        this.hasError = true;
+      },
+    });
+  }
+
+  onSearchChange() {
+    const term = this.searchTerm.toLowerCase();
+    this.filteredProducts = this.products.filter((product) =>
+      product.title.toLowerCase().includes(term)
+    );
+    this.applySorting();
+  }
+
+  onSortChange() {
+    this.applySorting();
+  }
+
+  applySorting() {
+    if (this.sortOption === 'lowToHigh') {
+      this.filteredProducts.sort((a, b) => a.price - b.price);
+    } else if (this.sortOption === 'highToLow') {
+      this.filteredProducts.sort((a, b) => b.price - a.price);
+    }
+  }
+  trackByFn(index: number, product: Product) {
+    return product.id;
   }
 }
